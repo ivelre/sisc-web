33904ac01ed7a47a1b28efe2bf35ae8a1d49e48d
Er Leví Media Rodríguez <ivelre@hotmail.com>
2018-06-15 13:31:06 -0500

    Update login

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\usuarioModel;
+use App\Models\evidenciaModel;
 
 class apiController extends Controller
 {
@@ -11,11 +12,22 @@ class apiController extends Controller
     	$response = new \stdClass;
     	$response -> status = false;
     	$response -> message = 'Tus credenciales no son las correctas para ingresar.';
-    	$user = usuarioModel::where('email',$req -> email)->first();
+    	$response -> usuario_id = null;
+			$response -> step = 1;
+    	$user = usuarioModel::where('email',$req -> casilla)->first();
     	if($user){
 				if(password_verify($req -> password, $user -> contrasena)){
+					$evidencias  = evidenciaModel::where('id_usuario',$user -> id)->where('id_tipo_evidencia',2)->first();
+					if($evidencias){
+						$response -> step = 2;
+						$evidencias  = evidenciaModel::where('id_usuario',$user -> id)->where('id_tipo_evidencia',5)->first();
+						if($evidencias)
+							$response -> step = 3;
+					}
 		    	$response -> status = true;
-		    	$response -> message = 'Haz iniciado sesión correctamente.';
+		    	$response -> message = 'Haz iniciado sesion correctamente.';
+		    	$response -> usuario_id = $user -> id;
+		    	$response -> tipo_usuario_id = $user -> id_rol;
 					return response()->json($response);
 				}
 			}
---
+++ app/Models/evidenciaModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class evidenciaModel extends Model
+{
+    protected $table = 'evidencias';
+    public $timestamps = false;
+}
